783854e3f8ff2ff09c15c10b399183cd1e2fc350
Henrik Engelbrink <hengel2810@gmail.com>
2018-02-10 16:07:26 +0100

    Added download

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const promBundle = require('express-prom-bundle');
+const path = require('path');
 const metricsMiddleware = promBundle({
     includeStatusCode: true,
     includeMethod: true,
@@ -21,5 +22,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.get('/download', function (req, res) {
+    var file = path.join(__dirname, 'DataRealm', 'default.realm');
+    res.download(file);
+});
 app.use('/', require('./src/routes'));
 app.listen(4000, () => console.log('Admin API running on port 4000'));
