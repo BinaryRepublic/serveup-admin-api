a24dc47b39f9cb7c95a9bcb7d773f0cabff7ffbc
Henrik Engelbrink <hengel2810@gmail.com>
2018-03-16 22:39:28 +0100

    Updated

--- app.js
+++ app.js
@@ -3,23 +3,16 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const promBundle = require('express-prom-bundle');
-// const path = require('path');
-const metricsMiddleware = promBundle({
-    includeStatusCode: true,
-    includeMethod: true,
-    includePath: true
-});
+
 const fileUpload = require('express-fileupload');
 
-app.use(metricsMiddleware);
 app.use('/', require('./src/middleware/accessControl').main);
-app.use(require('./src/middleware/authorization'));
-app.use(fileUpload());
+// app.use(require('./src/middleware/authorization'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(fileUpload());
 
 app.use('/', require('./src/authRoutes'));
 app.use(require('./src/middleware/authentication'));
