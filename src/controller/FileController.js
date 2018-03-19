5e1f78d1882b391a0d12b78ef935f97a4fdd97eb
Tobias Trinks <tobias.trinks@code.berlin>
2018-03-19 11:15:20 +0100

    stop script if upload files are missing

--- src/controller/FileController.js
+++ src/controller/FileController.js
@@ -6,22 +6,23 @@ class FileController {
     uploadRequest (req, res) {
         if (!req.files.file || !req.files.fileLock) {
             res.sendStatus(400);
+        } else {
+            req.files.file.mv('./DataRealm/default.realm', (err) => {
+                if (err) {
+                    console.log(err);
+                    res.sendStatus(500);
+                } else {
+                    req.files.fileLock.mv('./DataRealm/default.realm.lock', (err) => {
+                        if (err) {
+                            console.log(err);
+                            res.sendStatus(500);
+                        } else {
+                            res.sendStatus(200);
+                        }
+                    });
+                }
+            });
         }
-        req.files.file.mv('./DataRealm/default.realm', (err) => {
-            if (err) {
-                console.log(err);
-                res.sendStatus(500);
-            } else {
-                req.files.fileLock.mv('./DataRealm/default.realm.lock', (err) => {
-                    if (err) {
-                        console.log(err);
-                        res.sendStatus(500);
-                    } else {
-                        res.sendStatus(200);
-                    }
-                });
-            }
-        });
     };
     downloadRequest (req, res) {
         res.download('./DataRealm/default.realm');
