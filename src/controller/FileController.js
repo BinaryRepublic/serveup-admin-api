6049bc641ad6ee461dd0c2c5b1f169dcce0887dc
tobiastrinks <tobias.trinks@code.berlin>
2018-03-22 15:30:46 +0100

    added authorization

--- src/controller/FileController.js
+++ src/controller/FileController.js
@@ -9,33 +9,47 @@ class FileController {
     }
     uploadRequest (req, res) {
         // root only
-        // let authorization = this.authorization.request(req.accountId, false, false);
-        // if (authorization && !authorization.error) {
-        if (!req.files.file || !req.files.fileLock) {
-            res.sendStatus(400);
+        let authorization = this.authorization.request(req.accountId, false, false);
+        if (authorization && !authorization.error) {
+            if (!req.files.file || !req.files.fileLock) {
+                res.sendStatus(400);
+            } else {
+                req.files.file.mv('./DataRealm/default.realm', (err) => {
+                    if (err) {
+                        console.log(err);
+                        res.sendStatus(500);
+                    } else {
+                        req.files.fileLock.mv('./DataRealm/default.realm.lock', (err) => {
+                            if (err) {
+                                console.log(err);
+                                res.sendStatus(500);
+                            } else {
+                                res.sendStatus(200);
+                            }
+                        });
+                    }
+                });
+            }
         } else {
-            req.files.file.mv('./DataRealm/default.realm', (err) => {
-                if (err) {
-                    console.log(err);
-                    res.sendStatus(500);
-                } else {
-                    req.files.fileLock.mv('./DataRealm/default.realm.lock', (err) => {
-                        if (err) {
-                            console.log(err);
-                            res.sendStatus(500);
-                        } else {
-                            res.sendStatus(200);
-                        }
-                    });
-                }
-            });
+            return authorization;
         }
-        // } else {
-        //     return authorization;
-        // }
     };
     downloadRequest (req, res) {
-        res.download('./DataRealm/default.realm');
+        // root only
+        let authorization = this.authorization.request(req.accountId, false, false);
+        if (authorization && !authorization.error) {
+            if (!req.files.file || !req.files.fileLock) {
+                res.sendStatus(400);
+            } else {
+                res.download('./DataRealm/default.realm');
+            }
+        } else {
+            if (authorization) {
+                res.sendStatus(400).json(authorization);
+            } else {
+                res.sendStatus(400);
+            }
+        }
     }
 }
 module.exports = FileController;
