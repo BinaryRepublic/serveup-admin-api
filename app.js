62fb4017d8211fd8ae4723fcf1739d89bc39dc76
Henrik Engelbrink <hengel2810@gmail.com>
2018-03-25 19:28:26 +0200

    Added winston logging

--- app.js
+++ app.js
@@ -3,8 +3,18 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
+const expressWinston = require('express-winston');
+const winston = require('winston');
 const fileUpload = require('express-fileupload');
+
+app.use(expressWinston.logger({
+    transports: [
+        new winston.transports.Console({
+            json: true,
+            colorize: false
+        })
+    ]
+}));
 
 app.use('/', require('./src/middleware/accessControl').main);
 app.use(bodyParser.json());
@@ -12,9 +22,17 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(fileUpload());
-
 app.use('/', require('./src/authRoutes'));
 app.use(require('./src/middleware/authentication'));
-
 app.use('/', require('./src/routes'));
+
+app.use(expressWinston.errorLogger({
+    transports: [
+        new winston.transports.Console({
+            json: true,
+            colorize: false
+        })
+    ]
+}));
+
 module.exports = app.listen(4000, () => console.log('Admin API running on port 4000'));
