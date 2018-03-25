1bd248e8dc2c40215c5a40284958590bc4de80da
Henrik Engelbrink <hengel2810@gmail.com>
2018-03-25 19:58:30 +0200

    Updated logging

--- app.js
+++ app.js
@@ -3,18 +3,18 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const expressWinston = require('express-winston');
-const winston = require('winston');
+const { createLogger, transports, winston } = require('winston');
 const fileUpload = require('express-fileupload');
 
-app.use(expressWinston.logger({
+const logger = createLogger({
     transports: [
-        new winston.transports.Console({
-            json: true,
-            colorize: false
-        })
+        new transports.File({ filename: 'combined.log', level: 'info' })
+    ],
+    exceptionHandlers: [
+        new transports.File({ filename: 'exceptions.log' })
     ]
-}));
+});
+logger.exitOnError = false;
 
 app.use('/', require('./src/middleware/accessControl').main);
 app.use(bodyParser.json());
@@ -26,13 +26,21 @@ app.use('/', require('./src/authRoutes'));
 app.use(require('./src/middleware/authentication'));
 app.use('/', require('./src/routes'));
 
-app.use(expressWinston.errorLogger({
-    transports: [
-        new winston.transports.Console({
-            json: true,
-            colorize: false
-        })
-    ]
-}));
+app.use(function (req, res, next) {
+    var logObj = {
+        timestamp: new Date(),
+        status: req.statusCode,
+        route: req.path
+    };
+    if (req.statusCode !== 200) {
+        let errorRes = res.body;
+        if (errorRes && errorRes.error && errorRes.error.type && errorRes.error.msg) {
+            logObj.errorType = errorRes.error.type;
+            logObj.errorType = errorRes.error.msg;
+        }
+    }
+    logger.info(JSON.stringify(logObj));
+    next();
+});
 
 module.exports = app.listen(4000, () => console.log('Admin API running on port 4000'));
